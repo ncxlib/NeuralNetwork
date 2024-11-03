8625c55bf51dad47e1befd7de839cfe885647d48
chase coogan <chasecoogan12@gmail.com>
2024-11-03 14:35:08 -0500

    add neuron

--- NeuralNetwork/Model/Neuron.py
+++ NeuralNetwork/Model/Neuron.py
@@ -6,19 +6,23 @@ class Neuron:
         Initializes Neuron class:
             1. Weights - How much influence this input has on the overall outcome
             2. Bias - Added to the weighted sum of inputs. Helps shift activation Fn when weighted sum = 0.
-            3. set activation Fn
         '''
         self.weights = np.random.randn(n_inputs)
         self.bias = np.random.randn()
 
-    def calculate_neurons_weighted_sum(self, inputs):
+    def calculate_neurons_weighted_sum(self, inputs): 
         '''
-        Calculates the total weighted sum of all neuron inputs + the bias term
-        to later be passed to an activation Fn.
+        Params:
+            inputs - an array of features (should be an np array for dot prod)
 
-        Weighted Sum: z = w1⋅x_1+w_2⋅x_2+ ...+ w_n⋅x_n + b
-        x_i = each input
-        w_i = each weight
+        Returns:
+            The weighted sum as a float value.
+
+        Functionality:
+            Calculates the total weighted sum of all neuron inputs + the bias term,
+            which can later be passed to an activation function.
+
+            Weighted Sum: z = w1 ⋅ x1 + w2 ⋅ x2 + ...+ wn ⋅ xn + b
         '''
         weighted_sum = np.dot(self.weights, inputs) + self.bias
         return weighted_sum
