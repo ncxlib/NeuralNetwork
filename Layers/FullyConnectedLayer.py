909e9a89d443231f334812abc3f2b39d39a85453
Chase Coogan <47133441+cwcoogan@users.noreply.github.com>
2024-11-03 17:54:57 -0500

    Chase (#1)
    * layer abstract class
    
    * add FCL before laptop dies >.<
    
    * change folder struct

---
+++ Layers/FullyConnectedLayer.py
@@ -0,0 +1,19 @@
+import numpy as np
+import Layer
+from Neuron import Neuron
+
+class FullyConnectedLayer(Layer):
+    def __init__(self, n_inputs, n_neurons, activation_fn=None):
+        super().__init__(activation_fn)
+        self.n_inputs = n_inputs
+        self.n_neurons = n_neurons
+        self.initialize_params()
+
+    def initialize_params(self):
+        self.neurons = [Neuron(self.n_inputs) for _ in range(self.n_neurons)]
+
+    # keep track of weighted sum of all neurons
+    def forward_propogation(self):
+        pass
+
+    # apply activation fn
