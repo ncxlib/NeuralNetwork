a3a58cde2156e56df946ba1d7c350156762ad89c
Neel Patel <102969955+neelthepatel8@users.noreply.github.com>
2024-11-04 16:15:31 -0500

    fix module imports, add python gitignore. (#4)

--- neuralnetwork/Layers/FullyConnectedLayer.py
+++ neuralnetwork/Layers/FullyConnectedLayer.py
@@ -1,34 +1,35 @@
-from Layers.Layer import Layer
-from Neuron.Neuron import Neuron
+from neuralnetwork.Layers import Layer
+from neuralnetwork.Neuron import Neuron
 import numpy as np
 
+
 class FullyConnectedLayer(Layer):
-    def __init__(self, n_inputs : str , n_neurons : int, activation_fn : str):
+    def __init__(self, n_inputs: str, n_neurons: int, activation_fn: str):
         super().__init__(activation_fn)
         self.n_inputs = n_inputs
         self.n_neurons = n_neurons
         self.initialize_params()
 
     def initialize_params(self):
-        '''
+        """
         Initializes Neurons with random weights and biases following the Normal Distr.
-        '''
+        """
         self.neurons = [Neuron(self.n_inputs) for _ in range(self.n_neurons)]
 
     def forward_propagation(self, inputs):
-        '''
+        """
         inputs:
             An array of features (should be a numpy array)
-        
+
         Returns:
             An array of the output values from each neuron in the layer.
 
         Function:
             Performs forward propagation by calculating the weighted sum for each neuron
         and applying the activation function
-        '''
+        """
         activation_outputs = []
-        
+
         for neuron in self.neurons:
             weighted_sum = neuron.calculate_neuron_weighted_sum(inputs)
             output = self.activation_fn(weighted_sum)
--- neuralnetwork/Layers/Layer.py
+++ neuralnetwork/Layers/Layer.py
@@ -1,16 +1,19 @@
 from abc import ABC, abstractmethod
 import numpy as np
 
+
 class Layer(ABC):
-    def __init__(self, activation_fn : str):
+    def __init__(self, activation_fn: str):
         self.activation_fn = placeholder.get(activation_fn)
         if self.activation_fn is None:
-            raise ValueError("Missing activation function. Cannot be empty. Example: activation_fn=Relu")
+            raise ValueError(
+                "Missing activation function. Cannot be empty. Example: activation_fn=Relu"
+            )
 
     @abstractmethod
     def initialize_params(self):
         pass
 
     @abstractmethod
-    def forward_propagation(self):
+    def forward_propogation(self):
         pass
