caa05f8d97bc72f596c6d37069d096f68e5487e3
chase coogan <chasecoogan12@gmail.com>
2024-11-04 16:04:48 -0500

    fix imports and routes

--- Layers/Layer.py
+++ Layers/Layer.py
@@ -1,8 +1,9 @@
 from abc import ABC, abstractmethod
+import numpy as np
 
 class Layer(ABC):
     def __init__(self, activation_fn : str):
-        self.activation_fn = activation_fn.get(activation_fn)
+        self.activation_fn = placeholder.get(activation_fn)
         if self.activation_fn is None:
             raise ValueError("Missing activation function. Cannot be empty. Example: activation_fn=Relu")
 
@@ -11,5 +12,5 @@ class Layer(ABC):
         pass
 
     @abstractmethod
-    def forward_propogation(self):
+    def forward_propagation(self):
         pass
