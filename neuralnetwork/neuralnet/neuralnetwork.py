b23ea339250ebd5d2d56dfd8726dfd13b28e9f93
chase coogan <chasecoogan12@gmail.com>
2024-11-06 13:25:29 -0500

    add neuralnet class with add layer train predict forward back prop

--- neuralnetwork/neuralnet/neuralnetwork.py
+++ neuralnetwork/neuralnet/neuralnetwork.py
@@ -1,6 +1,33 @@
+from neuralnetwork.layers.fullyconnectedayer import FullyConnectedLayer
+
 class NeuralNetwork:
     def __init__(self):
-        pass
+        self.layers = []
+
+    def add_layer(self, layer):
+        self.layers.append(layer)
+
+    def forward_propagation(self, inputs):
+        for layer in self.layers:
+            inputs = layer.forward_propagation(inputs)
+        return inputs
+
+    def backward_propagate(self, y_orig, y_pred):
+        for layer in reversed(self.layers):
+            layer.back_propagation(y_orig, y_pred)
+    
+
+    def train(self, inputs, y_orig, epochs):
+        for epoch in range(epochs):
+            y_pred = self.forward_propagation(inputs)
+
+            loss = self.layers[-1].calculate_loss(y_pred, y_orig)
+            print(f"Epoch {epoch + 1}, Loss: {loss}")
+
+            self.backward_propagate(y_orig, y_pred)
+
+    def predict(self, inputs):
+        return self.forward_propagation(inputs)
 
     # TODO: Build out the neural net && all methods..
 
