e03985ded5baca4171891793834243ea15c5b60c
neelthepatel <patel.neel5@northeastern.edu>
2024-11-13 15:13:07 -0500

    fix init file

--- ncxlib/neuralnetwork/initializers/__init__.py
+++ ncxlib/neuralnetwork/initializers/__init__.py
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-
-=======
 from .initializer import ( Initializer, )
 from .he import (
 HeNormal,
@@ -9,4 +6,3 @@ HeNormal,
 from .zero import (
 Zero,
 )
->>>>>>> 6daac0421f948cd06d8059e0503ad873d2db33cf
